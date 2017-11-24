d8eab21aed3dcbf9d4d6e217ec6101900d9beb7e
keyosh0121 <k.guitar121@gmail.com>
2017-11-24 13:46:01 +0900

    removed odd migration files

--- db/migrate/20170831061855_band.rb
+++
@@ -1,6 +0,0 @@
-class Band < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :bands, :members
-    add_column :bands, :members, :string, array:true, default: []
-  end
-end
--- db/migrate/20170831062839_convert_members_of_bands_to_array.rb
+++
@@ -1,6 +0,0 @@
-class ConvertMembersOfBandsToArray < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :bands, :members
-    add_column :bands, :members, :text, default: [].to_yaml, array:true
-  end
-end
