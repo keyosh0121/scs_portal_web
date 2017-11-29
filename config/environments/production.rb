9329a7d912904a7b82527256f409259469f4dca1
keyosh0121 <k.guitar121@gmail.com>
2017-11-30 00:06:21 +0900

    entry: BETA Version 0.01

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,8 +67,8 @@ Rails.application.configure do
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
       :authentication => :plain,
-      :user_name      => 'app81566889@heroku.com',
-      :password       => 'x6ils8p18572',
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
       :domain         => 'heroku.com',
       :enable_starttls_auto => true
     }
