1e8df9ea9dc99c0788adf2d8765e27b616e64f81
keyosh0121 <k.guitar121@gmail.com>
2017-12-09 13:36:12 +0900

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171209034918) do
+ActiveRecord::Schema.define(version: 20171209043551) do
 
   create_table "bands", force: :cascade do |t|
     t.string "name"
