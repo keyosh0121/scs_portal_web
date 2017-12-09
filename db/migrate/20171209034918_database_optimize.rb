5fbbc41a8dd7d0939e1b092771666ae6806ec9b2
keyosh0121 <k.guitar121@gmail.com>
2017-12-09 13:23:47 +0900

    maintain

--- db/migrate/20171209034918_database_optimize.rb
+++ db/migrate/20171209034918_database_optimize.rb
@@ -2,5 +2,6 @@ class DatabaseOptimize < ActiveRecord::Migration[5.1]
   def change
     add_column :comments, :event_id, :integer
     add_column :mics, :band_id, :integer
+    change_column :mics, :status, :integer
   end
 end
