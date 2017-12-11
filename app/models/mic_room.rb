2d803a62d1c02675ca6c009b6265fa90e0983710
吉山慶 <k.guitar121@gmail.com>
2017-12-11 23:29:39 +0900

    microom adjust

--- app/models/mic_room.rb
+++ app/models/mic_room.rb
@@ -1,2 +1,11 @@
+class MicRoomValidator << ActiveModel::Validater
+	def validate(record)
+		if MicRoom.where(date: record.date).where(time: record.time).any?
+			record.errors[:base] << "既に部屋が登録されています"
+		end
+	end
+end
+
 class MicRoom < ApplicationRecord
+	validates_with MicRoomValidator
 end
