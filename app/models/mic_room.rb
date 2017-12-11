fd15f99bcc3595783b17812360d9867135e98cd4
吉山慶 <k.guitar121@gmail.com>
2017-12-11 23:45:54 +0900

    i found the bug

--- app/models/mic_room.rb
+++ app/models/mic_room.rb
@@ -1,4 +1,4 @@
-class MicRoomValidator << ActiveModel::Validater
+class MicRoomValidator < ActiveModel::Validater
 	def validate(record)
 		if MicRoom.where(date: record.date).where(time: record.time).any?
 			record.errors[:base] << "既に部屋が登録されています"
