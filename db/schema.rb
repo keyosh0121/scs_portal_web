a682e26bc626a5eedf3596bd4415602f0ab2b7b7
風太 <arafubeatbox@gmail.com>
2017-12-13 11:57:52 +0900

    commited db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -55,13 +55,6 @@ ActiveRecord::Schema.define(version: 20171209043551) do
     t.text "message"
   end
 
-  create_table "entry_events", force: :cascade do |t|
-    t.integer "event_id"
-    t.integer "type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "event_contents", force: :cascade do |t|
     t.string "name"
     t.string "event"
@@ -97,6 +90,7 @@ ActiveRecord::Schema.define(version: 20171209043551) do
     t.string "sender"
     t.date "date"
     t.string "time"
+    t.string "approval"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status"
@@ -151,7 +145,6 @@ ActiveRecord::Schema.define(version: 20171209043551) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
     t.string "name"
     t.string "email"
     t.string "tel"
