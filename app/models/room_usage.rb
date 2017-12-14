c6ea18d4d4a0f2aa82838517258090710eb71589
吉山慶 <k.guitar121@gmail.com>
2017-12-15 07:47:00 +0900

    auto deletion added(not yet scheduled)

--- app/models/room_usage.rb
+++ app/models/room_usage.rb
@@ -19,5 +19,8 @@ end
 class RoomUsage < ApplicationRecord
   serialize :period
   validates_with RoomUsageValidator
-
+	def self.delete_old_records
+		one_year_ago = Date.today - 365
+		self.where("date < one_year_ago").delete_all
+	end
 end
