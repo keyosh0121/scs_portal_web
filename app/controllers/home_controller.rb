d12ce62f43ce40650f5639502d5c27240a5acd7b
keyosh0121 <k.guitar121@gmail.com>
2017-12-20 00:15:42 +0900

    assets

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def top
     if @current_user
-      redirect_to('/user/#{@current_user.id}/show')
+      redirect_to( "/user/#{@current_user.id}/show")
     end
   end
 
