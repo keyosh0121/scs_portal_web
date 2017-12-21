ccc1fac411843a7b44dbe7bdc523a4176f619caf
吉山慶 <k.guitar121@gmail.com>
2017-12-21 16:47:31 +0900

    set current user modify

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,17 +4,11 @@ class ApplicationController < ActionController::Base
   before_action :set_current_user
 
   def set_current_user
-    if (user_id = session[:user_id])
-      @current_user = User.find(user_id)
-    elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: cookies.signed[:user_id])
-      if user && user.authenticated?(cookies[:remember_token])
-        @current_user = user
-      end
-    end
+    @current_user = User.find_by(id: session[:user_id])
   end
 
   def user_authentificate
+		#ログインしていない場合、もしくは管理者の承認が得られていない場合
     if @current_user == nil
       flash[:notice] = "ログインが必要な機能です。"
       redirect_to('/login')
@@ -24,6 +18,7 @@ class ApplicationController < ActionController::Base
     end
   end
 	def permit_access_only_for(authority)
+		#引数に指定された権限のユーザーのみアクセス可能にするメソッド
 		if @current_user.authority != authority
 			flash[:notice] = アクセス権限がありません
 			redirect_to('/user/#{@current_user.id}/show')
