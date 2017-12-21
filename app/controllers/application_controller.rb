c017ef60184d8e8275816d32798e68b0eb686e8a
keyosh0121 <k.guitar121@gmail.com>
2017-12-21 17:34:26 +0900

    send post is updated,please check it out, complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     if (user_id = session[:user_id])
       @current_user = User.find(user_id)
     elsif (user_id = cookies.signed[:user_id])
-      user = User.find(cookie(.signed[:user_id]))
+      user = User.find(cookies.signed[:user_id])
       if user && user.authenticated?(cookies[:remember_token])
         @current_user = user
       end
