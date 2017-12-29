4013a3372bd789a0012f629618bce822d487f49d
風太 <arafubeatbox@gmail.com>
2017-12-29 17:51:17 +0900

    trying again

--- app/models/room_usage.rb
+++ app/models/room_usage.rb
@@ -19,6 +19,7 @@ end
 class RoomUsage < ApplicationRecord
   serialize :period
   validates_with RoomUsageValidator
+  validates :date,  uniquness: { scope: [:period, :room]  }
 	def self.delete_old_records
 		one_year_ago = Date.today - 365
 		self.where("date < one_year_ago").delete_all
