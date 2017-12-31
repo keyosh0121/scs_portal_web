78b8c4e7d79a6af09a7bd9fb59071624b80c3593
風太 <arafubeatbox@gmail.com>
2017-12-31 14:24:00 +0900

    failed again

--- app/models/room_usage.rb
+++ app/models/room_usage.rb
@@ -1,8 +1,26 @@
+class RoomUsageValidator < ActiveModel::Validator
+  def validate(record)
+    if record.band == nil
+      record.errors[:base] << "バンド名が入力されていません"
+    end
+    if record.room == nil
+      record.errors[:base] << "部屋が選択されていません"
+    end
+    if record.date == nil
+      record.errors[:base] << "日付を選択してください"
+    end
+    if record.period == nil
+      record.errors[:base] << "時限を選択してください"
+    end
+
+  end
+end
+
 class RoomUsage < ApplicationRecord
   serialize :period
-  validates :date,  uniquness: { scope: [:period, :room]  }
-	def self.delete_old_records
-		one_year_ago = Date.today - 365
-		self.where("date < one_year_ago").delete_all
-	end
+  validates_with RoomUsageValidator
+  def self.delete_old_records
+    one_year_ago = Date.today - 365
+    self.where("date < one_year_ago").delete_all
+  end
 end
