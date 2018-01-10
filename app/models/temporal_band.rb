e16c5694601ae7634a6e4ad5650dc5ca8ea7cd6a
keyosh0121 <k.guitar121@gmail.com>
2018-01-10 17:22:12 +0900

    temporalband database model modify

--- app/models/temporal_band.rb
+++ app/models/temporal_band.rb
@@ -2,7 +2,7 @@ class TemporalBand < ApplicationRecord
   serialize :members
   validates :name, presence: true
   validates :event, presence: true
-  def members_database #TODO: membersに後で変更
+  def members #TODO: membersに後で変更
     TemporalBandMember.where(band_id: self.id).pluck('name')
   end
 end
