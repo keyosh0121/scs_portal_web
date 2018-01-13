e8f74b0d28af08c1be618c6b1920f74ecdce0dd0
keyosh0121 <k.guitar121@gmail.com>
2018-01-13 14:10:07 +0900

    temporally approve any registered accounts

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -47,7 +47,7 @@ class UserController < ApplicationController
       year: params[:year].to_i,
       univ: params[:univ],
       password: params[:password],
-      approved: true)
+      approval: true)
     if @user.save
       session[:user_id] = @user.id
       redirect_to("/")
