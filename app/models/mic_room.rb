59692a776ce966359d8d2d74f0b32a5f3a8a3236
吉山慶 <k.guitar121@gmail.com>
2018-03-20 15:04:17 +0900

    スプスプ楽しい

--- app/models/mic_room.rb
+++ app/models/mic_room.rb
@@ -1,9 +1,9 @@
 
 class MicRoom < ApplicationRecord
   validates :room_id, presence: {message: '部屋が選択されていません'}
+	belongs_to :room
   def reservation_type
     if self.reservation_type_num
-
       if self.reservation_type_num == 0
         return "当日予約"
       elsif self.reservation_type_num == 1
@@ -13,7 +13,6 @@ class MicRoom < ApplicationRecord
       else
         return "不明"
       end
-
     else
       return "不明"
     end
