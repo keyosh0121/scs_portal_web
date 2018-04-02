fd7e992e1ff341d87d978898ad7347552653f4be
風太 <arafubeatbox@gmail.com>
2018-04-03 01:31:34 +0900

    changed gitignore

--- db/schema.rb
+++ db/schema.rb
@@ -119,10 +119,10 @@ ActiveRecord::Schema.define(version: 20180325045313) do
     t.string "approval"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
+    t.integer "status"
     t.integer "band_id"
-    t.integer "period_id"
     t.integer "user_id"
+    t.integer "period_id"
     t.integer "order"
   end
 
