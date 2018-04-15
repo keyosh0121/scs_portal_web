f9903d2db7a40dad43846e85b159964734b00a32
風太 <arafubeatbox@gmail.com>
2018-04-15 21:37:11 +0900

    validation bring back

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,17 @@
 class Room < ApplicationRecord
   has_many :room_usages, dependent: :destroy
 	has_many :mic_rooms
+
+  def self.create_rooms
+    Room.create(name:"E1016", room_type:1)
+    Room.create(name:"E1116", room_type:1)
+    Room.create(name:"会議スペース", room_type:1)
+    Room.create(name:"B102", room_type:2)
+    Room.create(name:"B103", room_type:2)
+    Room.create(name:"B104", room_type:2)
+    Room.create(name:"B105", room_type:2)
+    Room.create(name:"B106", room_type:2)
+    Room.create(name:"B123", room_type:2)
+    Room.create(name:"B126", room_type:2)
+  end
 end
